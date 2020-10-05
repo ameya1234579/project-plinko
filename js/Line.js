be672f1347b06215162796e0e31574996c7aa3cc
ameya1234579 <65115166+ameya1234579@users.noreply.github.com>
2020-10-05 16:48:18 +0530

    Add files via upload

---
+++ js/Line.js
@@ -0,0 +1,18 @@
+class Line{
+    constructor(x,y,w,h) {
+      var options = {
+          isStatic: false,
+          restituion: 0
+      }
+      this.body = Bodies.rectangle(x,y,w,h,options);
+      this.w = w; 
+      this.h = h;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("yellow");
+    rect(pos.x, pos.y, this.w,this.h);
+    }
+  };
